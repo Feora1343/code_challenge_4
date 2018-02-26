a8255998fbc5efc12c9d4c3ffd1bbfe5d41fd4e8
Feora1343 <renee.vorbeck@gmail.com>
2018-02-26 14:50:43 -0600

    Fixed typos and syntax errors, base mode complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,14 @@
+
 var app = angular.module('StudentApp', []);
 
 /** ---- CONTROLLER HERE ---- **/
-app.controller('StudentController', function(){
+app.controller('StudentController', ['$http', function($http){
   // studentList is a global variable array of student names
   // don't overthink this, you don't need a $http request
   // the array already exists right here
 
   // Renee: "Using const self = this; just like I did in my weekend challenge"
   const self = this;
-
-  console.log(studentList);
   // @TODO: Write your controller code in here
 
   // Renee: "Having created the repo before the assignment was given to us,
@@ -24,7 +23,7 @@ app.controller('StudentController', function(){
 
   // Renee: "Let us GET the student array, shall we?"
   self.getStudents = function() {
-    $http:({
+    $http({
       // Renee: "Need to remember to use METHOD and not TYPE here."
       method: 'GET',
       url: '/students'
@@ -32,12 +31,25 @@ app.controller('StudentController', function(){
       // Renee: "Here we will assign the response.data to the empty self.studentList
       // array, now it won't be empty anymore and we can use it for our purposes."
       .then(function(response) {
-        self.studentList = response.data;
-      })
+        self.studentList = response.data;           
+    });
   }
 
   // Renee: "Now that we have our student list array, we need to call it like we have
   // called previous arrays before."
   self.getStudents();
 
-});
+  // Renee: "A lot of what I call the back end of the back end pieces have
+  // been set, now we need to get the show list function and then connected
+  // to the html with a ng-click. I'm using the toggle I used
+  // in my Weekend Challenge 4 assignment as a reference."
+
+  self.showList = function() {
+    self.hideList = !self.hideList;
+  }
+
+  // Renee: "I'm running into problems here because of my approach. 
+  // However, I think if just continue building out the app it will fix
+  // itself. Here I will make it sort when you press the button."
+  self.orderDirection = 'name';
+}])
--- server/server.js
+++ server/server.js
@@ -9,3 +9,18 @@ app.use(express.static('server/public'));
 app.listen(port, function (req, res) {
   console.log('Listening on port', port);
 });
+
+// Renee: "Since we're moving data.js to modules and created
+// a module to export, we need to include that here or else
+// things will not work and the grader of this assignment
+// will be sad."
+
+const studentData = require('./modules/data');
+
+// Renee: "Data needs to be able to go back and forth
+// through the get request and router. This should make
+// this possible."
+
+app.get('/students', function(req,res) {
+  res.send(studentData);
+})
